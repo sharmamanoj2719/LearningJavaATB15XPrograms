380a6cade7f9be2204caf6d213f0cbd3d481a632
sharmamanoj2719-gif <sharma.manoj2719@gmail.com>
2025-12-17 10:35:58 +0530

    FirstCommit

---
+++ src/ex_03_Java_Basics/Lab003_SingleLineComments.java
@@ -0,0 +1,9 @@
+package ex_03_Java_Basics;
+
+public class Lab003_SingleLineComments {
+    public static void main(String[]args){
+        // Single Line
+        System.out.println("Single Line Comment Program");
+
+    }
+}
---
+++ src/ex_03_Java_Basics/Lab004_MultiLineComments.java
@@ -0,0 +1,14 @@
+package ex_03_Java_Basics;
+
+public class Lab004_MultiLineComments {
+    /* 1.Multi Line Program
+    2.Learning Purpose
+         */
+    public static void main(String[]args){
+
+        System.out.println("MultiLineProgram");
+    }
+
+
+
+}
