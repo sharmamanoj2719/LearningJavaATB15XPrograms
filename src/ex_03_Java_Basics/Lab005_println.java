778a54ff6586fdad1e5603c70ba0d1edbcebee05
sharmamanoj2719-gif <sharma.manoj2719@gmail.com>
2025-12-18 12:20:21 +0530

    FirstCommit

---
+++ src/ex_03_Java_Basics/Lab005_println.java
@@ -0,0 +1,10 @@
+package ex_03_Java_Basics;
+
+public class Lab005_println {public static void main(String[]args)
+{
+
+   System.out.println("Use for new line insert in two statement");
+   System.out.println("2nd Line ,Use for new line insert in two statement");
+}
+
+}
