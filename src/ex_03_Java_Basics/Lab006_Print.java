adb14a1da410ed422614b458cb245953be9091b4
sharmamanoj2719-gif <sharma.manoj2719@gmail.com>
2025-12-18 12:29:08 +0530

    FirstCommit

---
+++ src/ex_03_Java_Basics/Lab006_Print.java
@@ -0,0 +1,8 @@
+package ex_03_Java_Basics;
+
+public class Lab006_Print {public static void main(String[]args)
+{
+    System.out.print("it is printed, but the line does not break");
+    System.out.print("2nd Line ,it is printed, but the line does not break ");
+}
+}
