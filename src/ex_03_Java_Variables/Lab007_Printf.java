472ed281c315fe76ee99109d46f9bbaf2acfa588
sharmamanoj2719-gif <sharma.manoj2719@gmail.com>
2025-12-24 07:50:27 +0530

    12/24/25

---
+++ src/ex_03_Java_Variables/Lab007_Printf.java
@@ -0,0 +1,23 @@
+package ex_03_Java_Variables;
+
+public class Lab007_Printf {
+    public static void main(String[] args) {
+        int a =10;
+
+        System.out.printf("Value of a is -> %d ",a);
+    }
+}
+/*
+        | Specifier | Use For      |
+        | --------- | ------------ |
+        | `%d`      | Integer      |
+        | `%s`      | String       |
+        | `%f`      | Float/double |
+        | `%c`      | Character    |
+        | `%b`      | Boolean      |
+        | `%x`      | Hex          |
+        | `%o`      | Octal        |
+        | `%e`      | Scientific   |
+*/
+
+/* %d int,byte,long,short, */
---
+++ src/ex_05_Java_Literals/Lab20_FloatLiterals.java
@@ -0,0 +1,13 @@
+package ex_05_Java_Literals;
+
+public class Lab20_FloatLiterals {
+    public static void main(String[] args) {
+        float price = 45.6f;     // Add 'f' to tell Java it is a float
+        float weight = 10.0f;
+        float temperature = -2.5f;
+        float discount = 0.75f;
+
+        System.out.println(price );
+
+    }
+}
