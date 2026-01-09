2c7a47caa7430d8039de0e1417cae7fed8086535
sharmamanoj2719-gif <sharma.manoj2719@gmail.com>
2026-01-09 07:59:10 +0530

    01/09/26-

---
+++ src/ex_13_WhileLoop/Lab_95_WhileLoop.java
@@ -0,0 +1,17 @@
+package ex_13_WhileLoop;
+
+public class Lab_95_WhileLoop {
+    public static void main(String[] args) {
+        int i = 0; // Init
+        while (i < 10) {//condition
+            System.out.println(i);
+            i++; // Updation
+        }
+
+        //  I, C, U
+        // for (int j = 0; j < 10 ; j++) {
+        //   System.out.println(j);
+        // }
+
+    }
+}
---
+++ src/ex_13_WhileLoop/Lab_96_While_OnlyCondition.java
@@ -0,0 +1,9 @@
+package ex_13_WhileLoop;
+
+public class Lab_96_While_OnlyCondition {
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("Hello, Manoj");  // infinite loop
+        }
+    }
+}
