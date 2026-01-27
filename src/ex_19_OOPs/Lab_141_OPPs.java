87b9535d0100121037b20d068dc541abb670e6b3
sharmamanoj2719-gif <sharma.manoj2719@gmail.com>
2026-01-27 18:11:03 +0530

    01/27/2026

---
+++ src/ex_19_OOPs/Lab_141_OPPs.java
@@ -0,0 +1,11 @@
+package ex_19_OOPs;
+
+public class Lab_141_OPPs {
+    public static void main(String[] args) {
+        person p1 = new person();  // p1 is object reference and // new person () is object
+        p1.name = "Manoj";
+
+       person p2 = new(person);
+       p2.name = "Krit";
+    }
+}
---
+++ src/ex_19_OOPs/person.java
@@ -0,0 +1,26 @@
+
+package ex_19_OOPs;
+
+public class person {
+
+    // A
+    String name;
+    int age;
+    String gender;
+    int salary;
+    long phone_no;
+    String courseName;
+    String paymentID;
+    boolean isMale;
+    boolean isMarried;
+    String[] address;
+
+
+    // B
+    void speak(){}
+    void eat(){}
+    void sleep(){};
+    void doAssignment(){};
+    void watchRecordings(){};
+
+}
